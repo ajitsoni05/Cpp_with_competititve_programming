658919f2a11d502d60402119291912b9d71ba49d
ajitsoni05 <49568689+ajitsoni05@users.noreply.github.com>
2021-12-04 21:21:29 +0530

    Create multiplicativeModuloInv.cpp

---
+++ Number_Theory/multiplicativeModuloInv.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Triplet{
+    public:
+    int x;
+    int y;
+    int gcd;
+};
+Triplet extendedEuclid(int a,int b)
+{
+    //Base Case (GCD)
+    if(b==0)
+    {
+        Triplet ans;
+        ans.gcd=a;
+        ans.x=1;
+        ans.y=0;
+        return ans;
+    }
+ //Extended Euclid
+ Triplet smallAns=extendedEuclid(b,a%b);
+  Triplet ans;
+  ans.x=smallAns.y;
+  ans.y=smallAns.x-(a/b)*smallAns.y;
+  ans.gcd=smallAns.gcd;
+  return ans;
+
+}
+int mmInverse(int a,int m)
+{
+    Triplet ans=extendedEuclid(a,m);
+    return ans.x;
+}
+int main()
+{
+   cout<<mmInverse(5,17)<<endl;
+
+}
